9edbd12d146731f9867e6bc4d81e60ae2e8c42f0
Raven412s <being.ashutosh16.20@gmail.com>
2025-08-21 12:56:13 +0530

    request callback form

--- app/api/enquiries/[id]/route.ts
+++ app/api/enquiries/[id]/route.ts
@@ -2,10 +2,11 @@ import { NextResponse } from "next/server";
 import Enquiry from "@/models/Enquiry";
 import { connectToDb } from "@/lib/mongodb";
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
     await connectToDb();
-    const { id } = params;
+    const awaitedParams = await params
+    const { id } = awaitedParams;
     const body = await req.json();
     const { called } = body;
     const updated = await Enquiry.findByIdAndUpdate(id, { called }, { new: true });
